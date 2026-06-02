cf6785c25fae3dcb2030682cc2e3900c062e16a1
Mike K <m.kokhanov.il@gmail.com>
2026-06-02 09:26:59 +0300

    [ misc ]: added script to VQA terminal colors

---
+++ scripts/misc/test.colors.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/env bash
+
+# -----------------------------------------------------------------------------
+
+cClear="\033[0m"
+
+cGreenBold="\033[1;32m"
+cGreen="\033[32m"
+cGreenBrightBold="\033[1;92m"
+cGreenBright="\033[92m"
+
+cYellowBold="\033[1;33m"
+cYellow="\033[33m"
+cYellowBrightBold="\033[1;93m"
+cYellowBright="\033[93m"
+
+cBlueBold="\033[1;34m"
+cBlue="\033[34m"
+cBlueBrightBold="\033[1;94m"
+cBlueBright="\033[94m"
+
+cRedBold="\033[1;31m"
+cRed="\033[31m"
+cRedBrightBold="\033[1;91m"
+cRedBright="\033[91m"
+
+cMagentaBold="\033[1;35m"
+cMagenta="\033[35m"
+cMagentaBrightBold="\033[1;95m"
+cMagentaBright="\033[95m"
+
+cCyanBold="\033[1;36m"
+cCyan="\033[36m"
+cCyanBrightBold="\033[1;96m"
+cCyanBright="\033[96m"
+
+cWhiteBold="\033[1;37m"
+cWhite="\033[37m"
+cWhiteBrightBold="\033[1;97m"
+cWhiteBright="\033[97m"
+
+
+# -----------------------------------------------------------------------------
+
+echo -e "[ ${cYellowBrightBold}COLORS$:${cClear} ]"
+echo ""
+echo -e "  - [${cGreenBrightBold} cGreenBrightBold ${cClear}]"
+echo -e "  - [${cGreenBright} cGreenBright ${cClear}]"
+echo -e "  - [${cGreenBold} cGreenBold ${cClear}]"
+echo -e "  - [${cGreen} cGreen ${cClear}]"
+echo ""
+echo -e "  - [${cYellowBrightBold} cYellowBrightBold ${cClear}]"
+echo -e "  - [${cYellowBright} cYellowBright ${cClear}]"
+echo -e "  - [${cYellowBold} cYellowBold ${cClear}]"
+echo -e "  - [${cYellow} cYellow ${cClear}]"
+echo ""
+echo -e "  - [${cRedBrightBold} cRedBrightBold ${cClear}]"
+echo -e "  - [${cRedBright} cRedBright ${cClear}]"
+echo -e "  - [${cRedBold} cRedBold ${cClear}]"
+echo -e "  - [${cRed} cRed ${cClear}]"
+echo ""
+echo -e "  - [${cBlueBrightBold} cBlueBrightBold ${cClear}]"
+echo -e "  - [${cBlueBright} cBlueBright ${cClear}]"
+echo -e "  - [${cBlueBold} cBlueBold ${cClear}]"
+echo -e "  - [${cBlue} cBlue ${cClear}]"
+echo ""
+echo -e "  - [${cMagentaBrightBold} cMagentaBrightBold ${cClear}]"
+echo -e "  - [${cMagentaBright} cMagentaBright ${cClear}]"
+echo -e "  - [${cMagentaBold} cMagentaBold ${cClear}]"
+echo -e "  - [${cMagenta} cMagenta ${cClear}]"
+echo ""
+echo -e "  - [${cCyanBrightBold} cCyanBrightBold ${cClear}]"
+echo -e "  - [${cCyanBright} cCyanBright ${cClear}]"
+echo -e "  - [${cCyanBold} cCyanBold ${cClear}]"
+echo -e "  - [${cCyan} cCyan ${cClear}]"
+echo ""
+echo -e "  - [${cWhiteBrightBold} cWhiteBrightBold ${cClear}]"
+echo -e "  - [${cWhiteBright} cWhiteBright ${cClear}]"
+echo -e "  - [${cWhiteBold} cWhiteBold ${cClear}]"
+echo -e "  - [${cWhite} cWhite ${cClear}]"
+echo ""
+echo -e "  - [${cClear} cClear ${cClear}]"
+echo ""
+
+# -----------------------------------------------------------------------------
+
+echo -e "[ ${cGreenBrightBold}DONE${cClear} ]"
+echo ""
+
+exit 0
