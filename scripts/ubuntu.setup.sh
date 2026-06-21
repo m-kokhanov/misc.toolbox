fc99fedb7fc8bde57891617acae8685fac6fb005
Mike K <m.kokhanov.il@gmail.com>
2026-06-21 09:45:01 +0300

    [ impr ]: ubuntu+gnome: initial setup script

--- scripts/ubuntu.setup.sh
+++ scripts/ubuntu.setup.sh
@@ -4,13 +4,15 @@
 
 cClear="\033[0m"
 
+cRedBold="\033[1;31m"
+cRed="\033[31m"
+cRedBrightBold="\033[1;91m"
+cRedBright="\033[91m"
+
 cGreenBold="\033[1;32m"
 cGreen="\033[32m"
 cGreenBrightBold="\033[1;92m"
 cGreenBright="\033[92m"
-
-cGrayBold="\033[1;38;5;250m"
-cGray="\033[38;5;250m"
 
 cYellowBold="\033[1;33m"
 cYellow="\033[33m"
@@ -22,17 +24,64 @@ cBlue="\033[34m"
 cBlueBrightBold="\033[1;94m"
 cBlueBright="\033[94m"
 
-cRedBold="\033[1;31m"
-cRed="\033[31m"
-cRedBrightBold="\033[1;91m"
-cRedBright="\033[91m"
+cMagentaBold="\033[1;35m"
+cMagenta="\033[35m"
+cMagentaBrightBold="\033[1;95m"
+cMagentaBright="\033[95m"
+
+cCyanBold="\033[1;36m"
+cCyan="\033[36m"
+cCyanBrightBold="\033[1;96m"
+cCyanBright="\033[96m"
+
+cWhiteBold="\033[1;37m"
+cWhite="\033[37m"
+cWhiteBrightBold="\033[1;97m"
+cWhiteBright="\033[97m"
+
+cBlackBold="\033[1;38m"
+cBlack="\033[38m"
+cBlackBrightBold="\033[1;98m"
+cBlackBright="\033[98m"
 
 # -----------------------------------------------------------------------------
 
-DEBUG=false
+DEBUG=true
+
+# -----------------------------------------------------------------------------
+
+print_success() {
+    local SUMMARY="$1"
+    local MSG="$2"
+
+    echo -e "${cClear}[${cGreenBrightBold} ${SUMMARY} ${cClear}] ${cGreenBright}${MSG}${cClear}"
+}
+
+print_error() {
+    local SUMMARY="$1"
+    local MSG="$2"
+
+    echo -e "${cClear}[${cRedBrightBold} ${SUMMARY} ${cClear}] ${cRedBright}${MSG}${cClear}"
+}
+
+print_warn() {
+    local SUMMARY="$1"
+    local MSG="$2"
+
+    echo -e "${cClear}[${cYellowBrightBold} ${SUMMARY} ${cClear}] ${cYellowBright}${MSG}${cClear}"
+}
+
+print_info() {
+    local SUMMARY="$1"
+    local MSG="$2"
+
+    echo -e "${cClear}[${cWhiteBold} ${SUMMARY} ${cClear}] ${cWhiteBright}${MSG}${cClear}"
+}
+
+# -----------------------------------------------------------------------------
 
 debug_print() {
-    echo -e "${cYellowBright}[ cmd ]: $@${cClear}"
+    echo -e "${cClear}[${cWhiteBrightBold} CMD ${cClear}]: ${cWhiteBright}$@${cClear}"
 }
 
 print_subject() {
@@ -41,11 +90,6 @@ print_subject() {
     echo -e "${cClear}[ ${cYellowBright}${topic}${cClear} ]: ${cGreen}${message}${cBlueBright}"
 }
 
-print_success() {
-    local summary=$1;
-    echo -e "${cClear}[ ${cGreen}${summary}${cClear} ]"
-    echo ""
-}
 
 print_failure() {
     local topic=$1
@@ -73,6 +117,8 @@ install_package() {
 # -----------------------------------------------------------------------------
 
 install_vagrant() {
+    [[ true = "${DEBUG}" ]] && return 0
+
     print_subject "PREPARING" "vagrant installation..."
 
     # wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
@@ -115,71 +161,3 @@ install_vagrant
 
 echo -e "[ ${cGreenBold}DONE${cClear} ]"
 echo ""
-
-# -----------------------------------------------------------------------------
-
-gnome_configure() {
-    local schema=$1
-    local key=$2
-    local value=$3
-
-    local current=$( gsettings get "${schema}" "${key}" )
-
-    echo -e "${cYellowBright}${schema}${cClear} ${cYellowBrightBold}${key} ${cGreen}${current}${cClear}"
-
-    if [[ true = "$DEBUG" ]];
-    then
-        debug_print gsettings set $schema $key "${value}"
-    else
-        gsettings set ${schema} ${key} "${value}"
-    fi
-}
-
-# -----------------------------------------------------------------------------
-
-echo ""
-echo -e "[ ${cYellowBrightBold}GNOME & TWEAKS${cClear} ] Setup..."
-echo ""
-
-
-# dock
-gnome_configure org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 32
-gnome_configure org.gnome.shell.extensions.dash-to-dock dock-position "'LEFT'"
-gnome_configure org.gnome.shell.extensions.dash-to-dock show-show-apps-button true
-gnome_configure org.gnome.shell.extensions.dash-to-dock show-trash true
-
-gnome_configure org.gnome.shell.extensions.dash-to-dock show-mounts-only-mounted true
-gnome_configure org.gnome.shell.extensions.dash-to-dock show-mounts-network true
-
-gnome_configure org.gnome.shell.extensions.dash-to-dock isolate-monitors false
-gnome_configure org.gnome.shell.extensions.dash-to-dock isolate-workspaces true
-
-echo ""
-
-# window management
-gnome_configure org.gnome.mutter center-new-windows true
-
-echo ""
-
-# desktop / workspaces
-gnome_configure org.gnome.mutter dynamic-workspaces false
-gnome_configure org.gnome.mutter workspaces-only-on-primary false
-gnome_configure org.gnome.desktop.wm.preferences num-workspaces 5
-
-echo ""
-
-# keyboard
-gnome_configure org.gnome.desktop.peripherals.keyboard delay "uint32 340"
-gnome_configure org.gnome.desktop.peripherals.keyboard repeat-interval "uint32 20"
-
-echo ""
-
-# update notifications
-gnome_configure com.ubuntu.update-notifier no-show-notifications true
-gnome_configure com.ubuntu.update-notifier regular-auto-launch-interval 90
-
-echo ""
-echo -e "[ ${cGreenBold}DONE${cClear} ]"
-echo ""
-
-# -----------------------------------------------------------------------------
