169fae3472cbfb51aff886435be6d9cfc959c8fe
Mike K <m.kokhanov.il@gmail.com>
2026-06-23 08:19:56 +0300

    [ impr ]: ubuntu+gnome: gnome settings

--- scripts/ubuntu.gnome.sh
+++ scripts/ubuntu.gnome.sh
@@ -76,8 +76,6 @@ print_info() {
 
 # -----------------------------------------------------------------------------
 
-# -----------------------------------------------------------------------------
-
 gnome_configure() {
     local schema=$1
     local key=$2
@@ -85,7 +83,7 @@ gnome_configure() {
 
     local current=$( gsettings get "${schema}" "${key}" )
 
-    echo -e "${cYellowBright}${schema}${cClear} ${cYellowBrightBold}${key} ${cGreen}${current}${cClear}"
+    echo -e "  ${cYellowBright}${schema}${cClear} ${cYellowBrightBold}${key} ${cGreen}${current}${cClear}"
 
     if [[ true = "$DEBUG" ]];
     then
@@ -102,7 +100,11 @@ print_warn "GNOME & TWEAKS" "Setup..."
 echo ""
 
 # dock
-gnome_configure org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 32
+
+print_info "INFO" "Dock"
+echo ""
+
+gnome_configure org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 36
 gnome_configure org.gnome.shell.extensions.dash-to-dock dock-position "'LEFT'"
 gnome_configure org.gnome.shell.extensions.dash-to-dock show-show-apps-button true
 gnome_configure org.gnome.shell.extensions.dash-to-dock show-trash true
@@ -116,37 +118,58 @@ gnome_configure org.gnome.shell.extensions.dash-to-dock isolate-workspaces true
 echo ""
 
 # window management
+
+print_info "INFO" "Window management"
+echo ""
+
 gnome_configure org.gnome.mutter center-new-windows true
 
 echo ""
 
 # desktop / workspaces
+
+print_info "INFO" "Desktop & Workspaces"
+echo ""
+
 gnome_configure org.gnome.mutter dynamic-workspaces false
 gnome_configure org.gnome.mutter workspaces-only-on-primary false
 gnome_configure org.gnome.desktop.wm.preferences num-workspaces 5
 
 echo ""
 
+# nautilus (file explorer)
+
+print_info "INFO" "Nautilus ( Files )"
+echo ""
+
+gnome_configure org.gnome.nautilus.preferences show-hidden-files true
+gnome_configure org.gnome.nautilus.preferences show-delete-permanently true
+gnome_configure org.gnome.nautilus.preferences default-folder-viewer 'list-view'
+
+echo ""
+
 # keyboard
+
+print_info "INFO" "Keyboard"
+echo ""
+
 gnome_configure org.gnome.desktop.peripherals.keyboard delay "uint32 340"
 gnome_configure org.gnome.desktop.peripherals.keyboard repeat-interval "uint32 20"
 
 echo ""
 
 # update notifications
+
+print_info "INFO" "Update nitifications"
+echo ""
+
 gnome_configure com.ubuntu.update-notifier no-show-notifications true
 gnome_configure com.ubuntu.update-notifier regular-auto-launch-interval 90
 
 echo ""
 
-# nautilus (file explorer)
-gnome_configure org.gnome.nautilus.preferences show-hidden-files true
-gnome_configure org.gnome.nautilus.preferences show-delete-permanently true
-gnome_configure org.gnome.nautilus.preferences default-folder-viewer 'list-view'
-
 # -----------------------------------------------------------------------------
 
-echo ""
 print_success "DONE" "Finished..."
 echo ""
 
